00c552a9712e08f90d195a6b449be2e1fa11558e
趙偉 <zwvista@msn.com>
2015-12-15 09:18:27 +0900

    searchActivity

--- app/src/main/java/com/zwstudio/lolly/android/SearchActivity.java
+++ app/src/main/java/com/zwstudio/lolly/android/SearchActivity.java
@@ -1,13 +1,30 @@
 package com.zwstudio.lolly.android;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.SearchView;
 
 public class SearchActivity extends BaseActivity {
+
+    SearchView svWord;
+    WebView wvOnline;
+    WebView wvOffline;
+
+    String word = "";
+    boolean webViewFinished = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        svWord = (SearchView)findViewById(R.id.svWord);
+        wvOnline = (WebView)findViewById(R.id.wvOnline);
+    }
+
+    private void searchDict(View view) {
+        word = svWord.getQuery().toString();
     }
 
 }
