a86eaccf02816bd4a08a56f11ed68d511b2ccf2e
趙偉 <zwvista@msn.com>
2016-08-13 19:41:57 +0900

    unitpart

--- app/src/main/java/com/zwstudio/lolly/android/SettingsActivity.java
+++ app/src/main/java/com/zwstudio/lolly/android/SettingsActivity.java
@@ -9,14 +9,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CheckedTextView;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.zwstudio.lolly.domain.TextBook;
 import com.zwstudio.lolly.domain.Dictionary;
 import com.zwstudio.lolly.domain.Language;
+import com.zwstudio.lolly.domain.TextBook;
 
 import java.util.List;
+import java.util.function.BooleanSupplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -46,7 +48,37 @@ public class SettingsActivity extends DrawerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        chkUnitTo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                chkUnitTo_onCheckedChanged(b);
+            }
+        });
         initSpnLanguage();
+    }
+
+    private void chkUnitTo_onCheckedChanged(boolean b) {
+        spnPartTo.setEnabled(b);
+        spnUnitTo.setEnabled(b);
+        if(chkUnitTo.isChecked())
+            updateUnitPartTo();
+    }
+
+    private void updateUnitPartFrom() {
+        TextBook m = getSettingsViewModel().getCurrentTextBook();
+        m.usunitfrom = m.usunitto;
+        spnUnitFrom.setSelection(spnUnitTo.getSelectedItemPosition());
+        m.uspartfrom = m.uspartto;
+        spnPartFrom.setSelection(spnPartTo.getSelectedItemPosition());
+    }
+
+    private void updateUnitPartTo() {
+        TextBook m = getSettingsViewModel().getCurrentTextBook();
+        m.usunitto = m.usunitfrom;
+        spnUnitTo.setSelection(spnUnitFrom.getSelectedItemPosition());
+        m.uspartto = m.uspartfrom;
+        spnPartTo.setSelection(spnPartFrom.getSelectedItemPosition());
     }
 
     private void initSpnLanguage() {
@@ -169,28 +201,31 @@ public class SettingsActivity extends DrawerActivity {
     }
 
     private void initUnitsAndParts() {
-        TextBook currentTextBook = getSettingsViewModel().getCurrentTextBook();
-        chkUnitTo.setChecked(currentTextBook.usunitfrom != currentTextBook.usunitto);
+        TextBook m = getSettingsViewModel().getCurrentTextBook();
+        BooleanSupplier isInvalidUnitPart = () -> m.usunitfrom * 10 + m.uspartfrom > m.usunitto * 10 + m.uspartto;
+        boolean b = m.usunitfrom != m.usunitto;
+        chkUnitTo.setChecked(b);
+        chkUnitTo_onCheckedChanged(b);
 
         {
-            List<String> lst = IntStream.rangeClosed(1, currentTextBook.units)
+            List<String> lst = IntStream.rangeClosed(1, m.units)
                     .mapToObj(i -> String.valueOf(i)).collect(Collectors.toList());
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_spinner_item, lst) {
                 @Override
                 public View getView(int position, View convertView, ViewGroup parent) {
                     View v = super.getView(position, convertView, parent);
-                    String m = lst.get(position);
+                    String s = lst.get(position);
                     TextView tv = (TextView) v.findViewById(android.R.id.text1);
-                    tv.setText(m);
+                    tv.setText(s);
                     return v;
                 }
                 @Override
                 public View getDropDownView(int position, View convertView, ViewGroup parent) {
                     View v = super.getDropDownView(position, convertView, parent);
-                    String m = lst.get(position);
+                    String s = lst.get(position);
                     CheckedTextView ctv = (CheckedTextView) v.findViewById(android.R.id.text1);
-                    ctv.setText(m);
+                    ctv.setText(s);
                     return v;
                 }
             };
@@ -198,28 +233,51 @@ public class SettingsActivity extends DrawerActivity {
             spnUnitFrom.setAdapter(adapter);
             spnUnitTo.setAdapter(adapter);
 
-            spnUnitFrom.setSelection(currentTextBook.usunitfrom - 1);
-            spnUnitTo.setSelection(currentTextBook.usunitto - 1);
+            spnUnitFrom.setSelection(m.usunitfrom - 1);
+            spnUnitTo.setSelection(m.usunitto - 1);
+
+            spnUnitFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    m.usunitfrom = position + 1;
+                    if(!chkUnitTo.isChecked() || isInvalidUnitPart.getAsBoolean())
+                        updateUnitPartTo();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
+            spnUnitTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    m.usunitto = position + 1;
+                    if(isInvalidUnitPart.getAsBoolean())
+                        updateUnitPartFrom();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
         }
 
         {
-            String[] lst = currentTextBook.parts.split(" ");
+            String[] lst = m.parts.split(" ");
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_spinner_item, lst) {
                 @Override
                 public View getView(int position, View convertView, ViewGroup parent) {
                     View v = super.getView(position, convertView, parent);
-                    String m = lst[position];
+                    String s = lst[position];
                     TextView tv = (TextView) v.findViewById(android.R.id.text1);
-                    tv.setText(m);
+                    tv.setText(s);
                     return v;
                 }
                 @Override
                 public View getDropDownView(int position, View convertView, ViewGroup parent) {
                     View v = super.getDropDownView(position, convertView, parent);
-                    String m = lst[position];
+                    String s = lst[position];
                     CheckedTextView ctv = (CheckedTextView) v.findViewById(android.R.id.text1);
-                    ctv.setText(m);
+                    ctv.setText(s);
                     return v;
                 }
             };
@@ -227,8 +285,31 @@ public class SettingsActivity extends DrawerActivity {
             spnPartFrom.setAdapter(adapter);
             spnPartTo.setAdapter(adapter);
 
-            spnPartFrom.setSelection(currentTextBook.uspartfrom - 1);
-            spnPartTo.setSelection(currentTextBook.uspartto - 1);
+            spnPartFrom.setSelection(m.uspartfrom - 1);
+            spnPartTo.setSelection(m.uspartto - 1);
+
+            spnPartFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    m.uspartfrom = position + 1;
+                    if(!chkUnitTo.isChecked() || isInvalidUnitPart.getAsBoolean())
+                        updateUnitPartTo();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
+            spnPartTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    m.uspartto = position + 1;
+                    if(isInvalidUnitPart.getAsBoolean())
+                        updateUnitPartFrom();
+                }
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                }
+            });
         }
     }
 
