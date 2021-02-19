f4f675088dfafd7a803d2c5bb1c5045b6520b6be
趙　偉 <zwvista@msn.com>
2021-02-19 16:00:37 +0900

    textbook

--- app/src/main/java/com/zwstudio/lolly/android/words/WordsTextbookDetailActivity.kt
+++ app/src/main/java/com/zwstudio/lolly/android/words/WordsTextbookDetailActivity.kt
@@ -1,13 +1,16 @@
 package com.zwstudio.lolly.android.words
 
 import android.app.Activity
+import android.view.LayoutInflater
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.zwstudio.lolly.android.R
+import com.zwstudio.lolly.android.databinding.ActivityWordsTextbookDetailBinding
 import com.zwstudio.lolly.data.misc.makeAdapter
+import com.zwstudio.lolly.data.words.WordsUnitDetailViewModel
 import com.zwstudio.lolly.data.words.WordsUnitViewModel
-import com.zwstudio.lolly.domain.misc.MSelectItem
 import com.zwstudio.lolly.domain.wpp.MUnitWord
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import org.androidannotations.annotations.*
@@ -18,36 +21,19 @@ class WordsTextbookDetailActivity : AppCompatActivity() {
 
     @Bean
     lateinit var vm: WordsUnitViewModel
+    lateinit var vmDetail: WordsUnitDetailViewModel
     lateinit var item: MUnitWord
 
-    @ViewById
-    lateinit var tvTextbookName: TextView
-    @ViewById
-    lateinit var tvID: TextView
     @ViewById
     lateinit var spnUnit: Spinner
     @ViewById
     lateinit var spnPart: Spinner
-    @ViewById
-    lateinit var etSeqNum: TextView
-    @ViewById
-    lateinit var tvWordID: TextView
-    @ViewById
-    lateinit var etWord: TextView
-    @ViewById
-    lateinit var etNote: TextView
-    @ViewById
-    lateinit var tvFamiID: TextView
-    @ViewById
-    lateinit var tvAccuracy: TextView
 
     val compositeDisposable = CompositeDisposable()
 
     @AfterViews
     fun afterViews() {
         item = intent.getSerializableExtra("word") as MUnitWord
-        tvTextbookName.text = "${resources.getString(R.string.label_textbook)} ${item.textbookname}"
-        tvID.text = "${resources.getString(R.string.label_id)} ${item.id}"
         run {
             val lst = item.textbook.lstUnits
             val adapter = makeAdapter(this, android.R.layout.simple_spinner_item, lst) { v, position ->
@@ -57,7 +43,6 @@ class WordsTextbookDetailActivity : AppCompatActivity() {
             }
             adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice)
             spnUnit.adapter = adapter
-            spnUnit.setSelection(item.textbook.lstUnits.indexOfFirst { it.value == item.unit })
         }
 
         run {
@@ -69,31 +54,21 @@ class WordsTextbookDetailActivity : AppCompatActivity() {
             }
             adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice)
             spnPart.adapter = adapter
-            spnPart.setSelection(vm.vmSettings.lstParts.indexOfFirst { it.value == item.part })
         }
-        etSeqNum.text = "${item.seqnum}"
-        tvWordID.text = "${resources.getString(R.string.label_wordid)} ${item.wordid}"
-        etWord.text = item.word
-        etNote.text = item.note
-        tvFamiID.text = "${resources.getString(R.string.label_famiid)} ${item.famiid}"
-        tvAccuracy.text = "${resources.getString(R.string.label_accuracy)} ${item.accuracy}"
-    }
 
-    @ItemSelect
-    fun spnUnitItemSelected(selected: Boolean, selectedItem: MSelectItem) {
-        item.unit = selectedItem.value
-    }
-
-    @ItemSelect
-    fun spnPartItemSelected(selected: Boolean, selectedItem: MSelectItem) {
-        item.part = selectedItem.value
+        DataBindingUtil.inflate<ActivityWordsTextbookDetailBinding>(LayoutInflater.from(this), R.layout.activity_words_textbook_detail,
+                findViewById(android.R.id.content), true).apply {
+            lifecycleOwner = this@WordsTextbookDetailActivity
+            vmDetail = WordsUnitDetailViewModel(item)
+            model = vmDetail
+        }
     }
 
     @OptionsItem
     fun menuSave() {
-        item.seqnum = etSeqNum.text.toString().toInt()
-        item.word = vm.vmSettings.autoCorrectInput(etWord.text.toString())
-        item.note = etNote.text.toString()
+        vmDetail.save(item)
+        item.word = vm.vmSettings.autoCorrectInput(item.word)
+        compositeDisposable.add(vm.update(item).subscribe())
         setResult(Activity.RESULT_OK)
         finish()
     }
