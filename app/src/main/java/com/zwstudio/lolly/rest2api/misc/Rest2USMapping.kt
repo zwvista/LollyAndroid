aa7c724e09adbb70613a5b25511a9181f81be564
趙偉 <zwvista@msn.com>
2021-03-05 01:26:09 +0900

    rx

--- app/src/main/java/com/zwstudio/lolly/rest2api/misc/Rest2USMapping.kt
+++
@@ -1,9 +0,0 @@
-package com.zwstudio.lolly.rest2api.misc
-
-import com.zwstudio.lolly.domain.misc.MUSMappings
-import retrofit2.http.GET
-
-interface Rest2USMapping {
-    @GET("USMAPPINGS")
-    suspend fun getData(): MUSMappings
-}
