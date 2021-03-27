9389c28120307f61c8ea44bb2a25d558d1b6c235
趙偉 <zwvista@msn.com>
2021-03-27 17:01:38 +0900

    nav 3

--- app/src/main/java/com/zwstudio/lolly/android/LollyApplication.kt
+++ app/src/main/java/com/zwstudio/lolly/android/LollyApplication.kt
@@ -6,6 +6,10 @@ import android.content.Context
 import android.content.DialogInterface
 import android.util.AttributeSet
 import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.navigation.fragment.findNavController
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.androidisland.vita.startVita
 import com.zwstudio.lolly.data.misc.SettingsViewModel
@@ -78,3 +82,29 @@ lateinit var vmSettings: SettingsViewModel
 fun <T> Observable<T>.applyIO(): Observable<T> =
     this.subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
+
+// https://stackoverflow.com/questions/50754523/how-to-get-a-result-from-fragment-using-navigation-architecture-component
+// https://stackoverflow.com/questions/56624895/android-jetpack-navigation-component-result-from-dialog/62054347#62054347
+fun <T> Fragment.setNavigationResult(value: T, key: String = "result") =
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, value)
+
+fun <T> Fragment.getNavigationResult(key: String = "result", onResult: (result: T) -> Unit) {
+    val navBackStackEntry = findNavController().currentBackStackEntry!!
+
+    val observer = LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_RESUME
+            && navBackStackEntry.savedStateHandle.contains(key)
+        ) {
+            val result = navBackStackEntry.savedStateHandle.get<T>(key)
+            result?.let(onResult)
+            navBackStackEntry.savedStateHandle.remove<T>(key)
+        }
+    }
+    navBackStackEntry.lifecycle.addObserver(observer)
+
+    viewLifecycleOwner.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_DESTROY) {
+            navBackStackEntry.lifecycle.removeObserver(observer)
+        }
+    })
+}
