2483779654dfd9136512e312d15756484284d9f9
趙偉 <zwvista@msn.com>
2021-04-04 18:52:40 +0900

    index

--- app/src/main/java/com/zwstudio/lolly/data/phrases/PhrasesUnitDetailViewModel.kt
+++ app/src/main/java/com/zwstudio/lolly/data/phrases/PhrasesUnitDetailViewModel.kt
@@ -7,16 +7,16 @@ import com.zwstudio.lolly.domain.wpp.MUnitPhrase
 class PhrasesUnitDetailViewModel(val item: MUnitPhrase) : ViewModel() {
     val id = MutableLiveData(item.id)
     val textbookname = MutableLiveData(item.textbookname)
-    val unitItemPosition = MutableLiveData(item.unitItemPosition)
-    val partItemPosition = MutableLiveData(item.partItemPosition)
+    val unitIndex = MutableLiveData(item.unitIndex)
+    val partIndex = MutableLiveData(item.partIndex)
     val seqnum = MutableLiveData(item.seqnum)
     val phrase = MutableLiveData(item.phrase)
     val translation = MutableLiveData(item.translation)
     val phraseid = MutableLiveData(item.phraseid)
 
     fun save() {
-        item.unitItemPosition = unitItemPosition.value!!
-        item.partItemPosition = partItemPosition.value!!
+        item.unitIndex = unitIndex.value!!
+        item.partIndex = partIndex.value!!
         item.seqnum = seqnum.value!!
         item.phrase = phrase.value!!
         item.translation = translation.value!!
