25671a20e3049582187a742243bb0d7f7b5f4bd7
趙偉 <zwvista@msn.com>
2021-04-14 08:11:03 +0900

    review

--- app/src/main/java/com/zwstudio/lolly/ui/words/WordsReviewFragment.kt
+++ app/src/main/java/com/zwstudio/lolly/ui/words/WordsReviewFragment.kt
@@ -1,27 +1,30 @@
 package com.zwstudio.lolly.ui.words
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import com.androidisland.vita.VitaOwner
 import com.androidisland.vita.vita
+import com.zwstudio.lolly.models.misc.MReviewOptions
 import com.zwstudio.lolly.ui.R
 import com.zwstudio.lolly.ui.databinding.FragmentWordsReviewBinding
 import com.zwstudio.lolly.ui.misc.autoCleared
 import com.zwstudio.lolly.ui.speak
 import com.zwstudio.lolly.viewmodels.words.WordsReviewViewModel
-import com.zwstudio.lolly.models.misc.MReviewOptions
 
 class WordsReviewFragment : Fragment() {
 
     val vm by lazy { vita.with(VitaOwner.Multiple(this)).getViewModel<WordsReviewViewModel>() }
     var binding by autoCleared<FragmentWordsReviewBinding>()
     var mAlreadyLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentWordsReviewBinding.inflate(inflater, container, false).apply {
@@ -34,11 +37,6 @@ class WordsReviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fun newTest() =
-            findNavController().navigate(R.id.action_wordsReviewFragment_to_reviewOptionsFragment,
-                bundleOf("options" to vm.options))
-
-        binding.btnNewTest.setOnClickListener { newTest() }
         binding.btnCheck.setOnClickListener { vm.check() }
         binding.chkSpeak.setOnClickListener {
             if (binding.chkSpeak.isChecked)
@@ -63,4 +61,22 @@ class WordsReviewFragment : Fragment() {
         vm.subscriptionTimer?.dispose()
         super.onDestroyView()
     }
+
+    fun newTest() =
+        findNavController().navigate(R.id.action_wordsReviewFragment_to_reviewOptionsFragment,
+            bundleOf("options" to vm.options))
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_new_test, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.menuNewTest -> {
+                newTest()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 }
