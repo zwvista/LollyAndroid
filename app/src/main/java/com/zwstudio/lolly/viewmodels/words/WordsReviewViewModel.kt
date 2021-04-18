2b96d06ef136e03816b1631f68dc99d8ab96dc99
趙偉 <zwvista@msn.com>
2021-04-18 23:48:39 +0900

    Is

--- app/src/main/java/com/zwstudio/lolly/viewmodels/words/WordsReviewViewModel.kt
+++ app/src/main/java/com/zwstudio/lolly/viewmodels/words/WordsReviewViewModel.kt
@@ -49,7 +49,7 @@ class WordsReviewViewModel(private val doTestAction: WordsReviewViewModel.() -> 
     val translationString = MutableLiveData("")
     val wordInputString = MutableLiveData("")
     val checkString = MutableLiveData("Check")
-    val searchIsEnabled = MutableLiveData(false)
+    val searchEnabled = MutableLiveData(false)
 
     fun newTest() = viewModelScope.launch {
         fun f() {
@@ -123,7 +123,7 @@ class WordsReviewViewModel(private val doTestAction: WordsReviewViewModel.() -> 
             else
                 incorrectVisible.value = true
             wordHintVisible.value = false
-            searchIsEnabled.value = true
+            searchEnabled.value = true
             checkString.value = "Next"
             if (!hasNext) return@launch
             val o = currentItem!!
@@ -154,7 +154,7 @@ class WordsReviewViewModel(private val doTestAction: WordsReviewViewModel.() -> 
         wordHintVisible.value = isTestMode
         translationString.value = ""
         wordInputString.value = ""
-        searchIsEnabled.value = false
+        searchEnabled.value = false
         doTestAction()
         if (hasNext) {
             indexString.value = "${index + 1}/$count"
