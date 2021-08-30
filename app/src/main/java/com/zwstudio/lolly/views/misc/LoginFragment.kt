6dbaf35b22b9c519968f833d4c32e34c99e2c7da
zwvista <zwvista@msn.com>
2021-08-30 11:58:26 +0900

    login

--- app/src/main/java/com/zwstudio/lolly/views/misc/LoginFragment.kt
+++ app/src/main/java/com/zwstudio/lolly/views/misc/LoginFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,7 @@ import com.androidisland.vita.VitaOwner
 import com.androidisland.vita.vita
 import com.zwstudio.lolly.viewmodels.misc.Global
 import com.zwstudio.lolly.viewmodels.misc.LoginViewModel
+import com.zwstudio.lolly.views.R
 import com.zwstudio.lolly.views.databinding.FragmentLoginBinding
 import kotlinx.coroutines.launch
 
@@ -21,6 +23,7 @@ class LoginFragment : Fragment() {
     var binding by autoCleared<FragmentLoginBinding>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = resources.getString(R.string.login)
         binding = FragmentLoginBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
             model = vm
