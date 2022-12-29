14c5eed899efdbcd2f7b080a92573961072fce6f
趙偉 <>
2022-12-29 19:16:49 +0900

    compose

---
+++ compose/src/main/java/com/zwstudio/lolly/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.zwstudio.lolly
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.zwstudio.lolly.services.misc.tts
+import com.zwstudio.lolly.ui.theme.LollyAndroidTheme
+import com.zwstudio.lolly.views.Drawer
+import com.zwstudio.lolly.views.DrawerScreens
+import com.zwstudio.lolly.views.misc.SearchScreen
+import com.zwstudio.lolly.views.misc.SettingsScreen
+import com.zwstudio.lolly.views.patterns.PatternsScreen
+import com.zwstudio.lolly.views.phrases.PhrasesUnitScreen
+import com.zwstudio.lolly.views.words.WordsUnitScreen
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LollyAndroidTheme {
+                AppMainScreen()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tts.shutdown()
+    }
+}
+
+@Composable
+fun AppMainScreen() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colors.background) {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        val openDrawer = {
+            scope.launch {
+                drawerState.open()
+            }
+        }
+        ModalDrawer(
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                Drawer(
+                    onDestinationClicked = { route ->
+                        scope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId)
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = DrawerScreens.Search.route
+            ) {
+                composable(DrawerScreens.Search.route) {
+                    SearchScreen(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Settings.route) {
+                    SettingsScreen(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.WordsUnit.route) {
+                    WordsUnitScreen(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.PhrasesUnit.route) {
+                    PhrasesUnitScreen(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Patterns.route) {
+                    PatternsScreen(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LollyAndroidTheme {
+        AppMainScreen()
+    }
+}
---
+++ compose/src/main/java/com/zwstudio/lolly/views/patterns/PatternsScreen.kt
@@ -0,0 +1,39 @@
+package com.zwstudio.lolly.views.patterns
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zwstudio.lolly.ui.theme.LollyAndroidTheme
+import com.zwstudio.lolly.views.TopBar
+
+@Composable
+fun PatternsScreen(openDrawer: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(
+            title = "Patterns",
+            buttonIcon = Icons.Filled.Menu,
+            onButtonClicked = { openDrawer() }
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Home Page content here.")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PatternsScreenPreview() {
+    LollyAndroidTheme {
+        PatternsScreen() {}
+    }
+}
