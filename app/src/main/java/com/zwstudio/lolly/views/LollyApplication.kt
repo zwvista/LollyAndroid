6457145cc91fa696ed61543e85766225d34c334c
趙偉 <>
2022-12-29 20:03:00 +0900

    onCreateApp

--- app/src/main/java/com/zwstudio/lolly/views/LollyApplication.kt
+++ app/src/main/java/com/zwstudio/lolly/views/LollyApplication.kt
@@ -8,78 +8,14 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.androidisland.vita.startVita
-import com.zwstudio.lolly.services.misc.*
-import com.zwstudio.lolly.services.wpp.*
-import com.zwstudio.lolly.viewmodels.misc.SettingsViewModel
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Completable
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.schedulers.Schedulers
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
-import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
-import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.zwstudio.lolly.services.misc.onCreateApp
 
 class LollyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
-        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-        retrofitJson = Retrofit.Builder().baseUrl("https://zwvista.com/lolly/api.php/records/")
-            .client(client)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        retrofitSP = Retrofit.Builder().baseUrl("https://zwvista.com/lolly/sp.php/")
-            .client(client)
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        // https://futurestud.io/tutorials/retrofit-2-receive-plain-string-responses
-        retrofitHtml = Retrofit.Builder().baseUrl("https://www.google.com")
-            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .build()
-        vmSettings = SettingsViewModel()
         startVita()
-        // https://github.com/InsertKoinIO/koin/issues/1076
-        startKoin {
-            // Koin Android logger
-            androidLogger(Level.ERROR)
-            //inject Android context
-            androidContext(this@LollyApplication)
-            // use modules
-            modules(lollyModule)
-        }
-    }
-    val lollyModule = module {
-        single { AutoCorrectService() }
-        single { DictionaryService() }
-        single { HtmlService() }
-        single { LanguageService() }
-        single { TextbookService() }
-        single { UserService() }
-        single { UserSettingService() }
-        single { USMappingService() }
-        single { VoiceService() }
-
-        single { LangPhraseService() }
-        single { LangWordService() }
-        single { PatternService() }
-        single { PatternWebPageService() }
-        single { UnitPhraseService() }
-        single { UnitWordService() }
-        single { WebPageService() }
-        single { WordFamiService() }
+        onCreateApp(this)
     }
 }
 
--- lollylib/src/main/java/com/zwstudio/lolly/services/misc/CommonService.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/services/misc/CommonService.kt
@@ -1,13 +1,25 @@
 package com.zwstudio.lolly.services.misc
 
+import android.content.Context
 import android.speech.tts.TextToSpeech
+import com.zwstudio.lolly.services.wpp.*
 import com.zwstudio.lolly.viewmodels.misc.SettingsViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object Global {
     var userid = ""
@@ -68,6 +80,57 @@ lateinit var retrofitSP: Retrofit
 lateinit var retrofitHtml: Retrofit
 lateinit var vmSettings: SettingsViewModel
 lateinit var tts: TextToSpeech
+
+fun onCreateApp(context: Context) {
+    val interceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+    val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+    retrofitJson = Retrofit.Builder().baseUrl("https://zwvista.com/lolly/api.php/records/")
+        .client(client)
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    retrofitSP = Retrofit.Builder().baseUrl("https://zwvista.com/lolly/sp.php/")
+        .client(client)
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    // https://futurestud.io/tutorials/retrofit-2-receive-plain-string-responses
+    retrofitHtml = Retrofit.Builder().baseUrl("https://www.google.com")
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .build()
+    vmSettings = SettingsViewModel()
+    // https://github.com/InsertKoinIO/koin/issues/1076
+    startKoin {
+        // Koin Android logger
+        androidLogger(Level.ERROR)
+        //inject Android context
+        androidContext(context)
+        // use modules
+        modules(lollyModule)
+    }
+}
+
+val lollyModule = module {
+    single { AutoCorrectService() }
+    single { DictionaryService() }
+    single { HtmlService() }
+    single { LanguageService() }
+    single { TextbookService() }
+    single { UserService() }
+    single { UserSettingService() }
+    single { USMappingService() }
+    single { VoiceService() }
+
+    single { LangPhraseService() }
+    single { LangWordService() }
+    single { PatternService() }
+    single { PatternWebPageService() }
+    single { UnitPhraseService() }
+    single { UnitWordService() }
+    single { WebPageService() }
+    single { WordFamiService() }
+}
 
 fun speak(text: String) =
     tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
