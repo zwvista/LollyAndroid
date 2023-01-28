505a2eda24b1821b6af55aaac948ca0c6b215de8
趙偉 <zwvista@msn.com>
2023-01-28 10:56:18 +0900

    version

--- app/src/main/java/com/zwstudio/lolly/ui/phrases/PhrasesUnitFragment.kt
+++ app/src/main/java/com/zwstudio/lolly/ui/phrases/PhrasesUnitFragment.kt
@@ -1,13 +1,11 @@
 package com.zwstudio.lolly.ui.phrases
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.*
 import android.widget.ImageView
 import android.widget.SearchView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.setFragmentResultListener
@@ -36,7 +34,6 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.nio.file.Files.delete
 
 class PhrasesUnitFragment : DrawerListFragment(), MenuProvider {
 
