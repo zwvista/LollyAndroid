db8cb560734d9302faf3c143827c56902cad03c1
趙偉 <zwvista@msn.com>
2024-09-03 07:56:31 +0900

    kotlin 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    extra.apply {
-        set("kotlin_version", "1.9.0")
-        set("nav_version", "2.7.7")
-        set("retrofit_version", "2.11.0")
-        set("lifecycle_version", "2.8.4")
-        set("koin_version", "3.5.6")
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.5.2")
-        classpath(kotlin("gradle-plugin", version = "${rootProject.extra["kotlin_version"]}"))
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${rootProject.extra["nav_version"]}")
+        classpath(libs.gradle)
+        classpath(kotlin("gradle-plugin", version = "2.0.20"))
+        classpath(libs.navigation.safe.args.gradle.plugin)
     }
 }
 allprojects {
