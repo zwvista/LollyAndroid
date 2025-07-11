02eab287c9d431310313decfd625af3aada085d2
zwvista <zwvista@msn.com>
2025-07-11 15:10:58 +0900

    blogs 4

---
+++ lollylib/src/main/java/com/zwstudio/lolly/models/blogs/MLangBlogPost.kt
@@ -0,0 +1,27 @@
+package com.zwstudio.lolly.models.blogs
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.IgnoredOnParcel
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
+
+data class MLangBlogPosts(
+    @SerializedName("records")
+    var lst: List<MLangBlogPost>? = null
+)
+
+@Parcelize
+data class MLangBlogPost(
+    @SerializedName("ID")
+    var id: Int = 0,
+    @SerializedName("LANGID")
+    var langid: Int = 0,
+    @SerializedName("TITLE")
+    var title: String = "",
+    @SerializedName("URL")
+    var url: String = ""
+) : Serializable, Parcelable {
+    @IgnoredOnParcel
+    @Transient var gpid = 0
+}
