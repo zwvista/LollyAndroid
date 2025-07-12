e1487e68b98aa29c88f5fa405374cf79826f66df
趙偉 <zwvista@msn.com>
2025-07-12 17:28:07 +0900

    api 5

--- lollylib/src/main/java/com/zwstudio/lolly/restapi/blogs/RestUnitBlogPost.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/restapi/blogs/RestUnitBlogPost.kt
@@ -4,7 +4,6 @@ import com.zwstudio.lolly.models.blogs.MUnitBlogPost
 import com.zwstudio.lolly.models.blogs.MUnitBlogPosts
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.Body
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -15,11 +14,9 @@ interface RestUnitBlogPost {
     @GET("UNITBLOGPOSTS")
     fun getDataByTextbook(@Query("filter") vararg filters: String): Single<MUnitBlogPosts>
 
-    @FormUrlEncoded
     @PUT("UNITBLOGPOSTS/{id}")
     fun update(@Path("id") id: Int, @Body item: MUnitBlogPost): Single<Int>
 
-    @FormUrlEncoded
     @POST("UNITBLOGPOSTS")
     fun create(@Body item: MUnitBlogPost): Single<Int>
 }
--- lollylib/src/main/java/com/zwstudio/lolly/services/misc/HtmlService.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/services/misc/HtmlService.kt
@@ -1,12 +1,12 @@
 package com.zwstudio.lolly.services.misc
 
 import com.zwstudio.lolly.common.applyIO
-import com.zwstudio.lolly.common.retrofitJson
+import com.zwstudio.lolly.common.retrofitHtml
 import com.zwstudio.lolly.restapi.misc.RestHtml
 import io.reactivex.rxjava3.core.Single
 
 class HtmlService {
-    private val apiHtml = retrofitJson.create(RestHtml::class.java)
+    private val apiHtml = retrofitHtml.create(RestHtml::class.java)
 
     // https://futurestud.io/tutorials/retrofit-2-receive-plain-string-responses
     fun getHtml(url: String): Single<String> =
