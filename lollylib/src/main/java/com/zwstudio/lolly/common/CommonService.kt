1ee44726dc2faf6e40a500221172e38a965b11fe
趙偉 <zwvista@msn.com>
2025-07-12 14:11:29 +0900

    koin module

--- lollylib/src/main/java/com/zwstudio/lolly/common/CommonService.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/common/CommonService.kt
@@ -54,9 +54,9 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
