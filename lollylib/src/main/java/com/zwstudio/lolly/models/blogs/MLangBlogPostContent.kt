d484c9905aff2e1f21b2b1f9c1f70ac0e22ca700
趙偉 <zwvista@msn.com>
2025-07-16 00:10:21 +0900

    MLangBlogPostContent

--- lollylib/src/main/java/com/zwstudio/lolly/models/blogs/MLangBlogPostContent.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/models/blogs/MLangBlogPostContent.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
-data class MLangBlogsContent(
+data class MLangBlogPostContents(
     @SerializedName("records")
     var lst: List<MLangBlogPostContent> = emptyList()
 )
--- lollylib/src/main/java/com/zwstudio/lolly/restapi/blogs/RestLangBlogPostContent.kt
+++ lollylib/src/main/java/com/zwstudio/lolly/restapi/blogs/RestLangBlogPostContent.kt
@@ -1,12 +1,16 @@
 package com.zwstudio.lolly.restapi.blogs
 
 import com.zwstudio.lolly.models.blogs.MLangBlogPostContent
-import com.zwstudio.lolly.models.blogs.MLangBlogsContent
-import retrofit2.http.*
+import com.zwstudio.lolly.models.blogs.MLangBlogPostContents
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RestLangBlogPostContent {
     @GET("LANGBLOGPOSTS")
-    suspend fun getDataById(@Query("filter") filter: String): MLangBlogsContent
+    suspend fun getDataById(@Query("filter") filter: String): MLangBlogPostContents
 
     @PUT("LANGBLOGPOSTS/{id}")
     suspend fun update(@Path("id") id: Int, @Body item: MLangBlogPostContent): Int
